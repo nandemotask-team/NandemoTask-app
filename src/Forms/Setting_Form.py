309127899e2c2b148a3eb84a69acf69f97874120
ElijahLee03 <eljlee0320@gmail.com>
2025-04-13 02:00:12 +0900

    익스탠션 추가 부분 개발(아직 Add 버튼으로 zip 파일 해제 밑 work를 등록하는 과정에 대한 개발이 필요)

--- src/Forms/Setting_Form.py
+++ src/Forms/Setting_Form.py
@@ -2,7 +2,7 @@ import sys, os
 from PySide6.QtWidgets import QMainWindow, QFileDialog
 from ui.Setting_Form_ui import Ui_Setting_Form
 
-from src.DataControl.AddressData import return_task_address, write_task_address
+from src.DataControl.AddressData import return_task_address, write_task_address, write_work_address, return_work_address
 
 class Setting_Form(QMainWindow, Ui_Setting_Form):
     def __init__(self):
@@ -11,19 +11,30 @@ class Setting_Form(QMainWindow, Ui_Setting_Form):
 
         # 기본정보 세팅
         TaskAddress = return_task_address()
+        WorkAddress = return_work_address()
         self.TasksAddres_lineEdit.setText(TaskAddress)
+        self.WorksAddres_lineEdit.setText(WorkAddress)
         
         # 세팅 옵션션
         self.FindTasksAddress_BT.clicked.connect(self.FindTasksAddress_BT_f)
+        self.FindWorksAddress_BT.clicked.connect(self.FindWorksAddress_BT_f)
     
         self.Ok_BT.clicked.connect(self.Ok_BT_f)
+
     def FindTasksAddress_BT_f(self):
         NewTaskAddress = QFileDialog.getExistingDirectory(self, self.tr("Open Data files"), "./", QFileDialog.ShowDirsOnly)
         # write_task_address(NewTaskAddress)
         self.TasksAddres_lineEdit.setText(NewTaskAddress)
+    
+    def FindWorksAddress_BT_f(self):
+        NewWorkAddress = QFileDialog.getExistingDirectory(self, self.tr("Open Data files"), "./", QFileDialog.ShowDirsOnly)
+        self.WorksAddres_lineEdit.setText(NewWorkAddress)
+
 
     def Ok_BT_f(self):
         TaskAddress = self.TasksAddres_lineEdit.text()
+        WorkAddress = self.WorksAddres_lineEdit.text()
         write_task_address(TaskAddress)
+        write_work_address(WorkAddress)
         self.close()
         # TasksAddres_lineEdit 에 TaskAddress 업데이트 하는 코드 제작 필요
